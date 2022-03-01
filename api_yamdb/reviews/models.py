11771d911f0c7e323c8b4e645118196fcfe2b797
Семенов Евгений Игоревич <Semenov.EI@GRP.LOCAL>
2022-03-01 14:12:29 +0300

    changed User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+ROLE_CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='Адрес электронной почты'
+    )
+    bio = models.TextField(
+        blank=True,
+        verbose_name='Биография'
+    )
+    role = models.CharField(
+        choices=ROLE_CHOICES,
+        default='user',
+        verbose_name='Роль'
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
+
+    @property
+    def is_user(self):
+        return self.role == 'user'
