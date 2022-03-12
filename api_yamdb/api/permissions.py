84d2a24f9d9b7c8ac8e14f9b528314da9d5907cd
pro100cemuk <pro100cemuk@gmail.com>
2022-03-12 19:45:34 +0300

    changed permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,31 +7,18 @@ class IsRoleAdmin(permissions.BasePermission):
         user = request.user
         return user.is_authenticated and user.is_admin
 
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        return user.is_authenticated and user.is_admin
 
-
-class IsRoleModerator(permissions.BasePermission):
+class IsAuthorAdminModeratorOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         user = request.user
-        return user.is_authenticated and user.is_moderator
-
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        return user.is_authenticated and user.is_moderator
-
-
-class IsAuthorOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        user = request.user
-        return (user.is_authenticated and user.is_user
+        return (user.is_authenticated
                 or request.method in permissions.SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
         return (obj.author == request.user
+                or request.user.is_authenticated and request.user.is_admin
+                or request.user.is_authenticated and request.user.is_moderator
                 or request.method in permissions.SAFE_METHODS)
 
 
