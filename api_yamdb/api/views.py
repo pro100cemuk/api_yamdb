966ed5327fb887b5e5a53f0b6b7f9d6e4b5b1f92
Nykolay Taranets <nvtaranets@gmail.com>
2022-03-13 13:50:08 +0300

    Correct pep8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, serializers, status, viewsets, mixins
 from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
